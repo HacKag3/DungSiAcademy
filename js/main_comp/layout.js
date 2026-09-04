221086d73bb7c9dd43fb21b843d4ca624a011750
HacKag3 <bachhac.kage@gmail.com>
2026-09-04 02:25:33 +0200

    aggiunta dell'allert di sito in fase di sviluppo

--- js/main_comp/layout.js
+++ js/main_comp/layout.js
@@ -1,8 +1,27 @@
 import { loadHeader } from "./header.js";
 import { loadFooter } from "./footer.js";
 
+function loadDevelopmentAlert() {
+    if (document.querySelector(".development-alert")) {
+        return;
+    }
+
+    const alertEl = document.createElement("div");
+    alertEl.className = "development-alert";
+    alertEl.setAttribute("role", "alert");
+    alertEl.textContent = "Sito ancora in fase di sviluppo: le informazioni potrebbero non essere complete o fittizie.";
+
+    const headerEl = document.querySelector("header");
+    if (headerEl) {
+        headerEl.after(alertEl);
+    } else {
+        document.body.prepend(alertEl);
+    }
+}
+
 export function loadLayout() {
     loadHeader();
+    loadDevelopmentAlert();
     loadFooter();
 }
 
